2c12ec6433037d0b09726987568ce047578f9a5c
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-10-22 00:11:53 +0300

    Feat(TS) Homework part 2 complete

--- src/2.ts
+++ src/2.ts
@@ -7,7 +7,6 @@
   властивість department, яка буде доступна лише всередині класу Employee.
   salary, яке буде доступне лише всередині класу Employee та його підкласів.
 
-
   Клас Manager повинен бути підклас класу Employee
 
   Необхідно реалізувати в класі Manager конструктор, який викликатиме конструктор суперкласу та збільшуватиме salary на 10000.
@@ -16,11 +15,11 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
-
-  constructor(name: string, department: string, salary: number) {
+  constructor(
+    public name: string,
+    private department: string,
+    protected salary: number,
+  ) {
     this.name = name;
     this.department = department;
     this.salary = salary;
@@ -33,7 +32,9 @@ class Employee {
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary + 10000);
+  }
 }
-
 
 export {};
